8bf6358131658be53d13ee3ea5bbed1c147a6ef4
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-02-25 00:27:32 +0800

    modal added by raihan

---
+++ js/quz.js
@@ -0,0 +1,29 @@
+// //Q1: If Fake James gives you the bondCode variable and tells you to find the code in javascript , what will you get?
+
+// const bondCode=` I am Fake James bond . My new code is: 00${7+1+2}`
+// console.log('the bondCodeis :', bondCode)
+
+// //Q2: How would you access "sakib khan" ?
+
+// const data={ result: [ {userName: {title:'Mr.',name:"sakib khan"} } ] }
+
+// const findName= data.result[0].userName.name
+// console.log(findName)
+
+
+// // Q3: Which array method will you use to obtain an array containing the cubes of array p? 
+// const p=[1,2,3]; 
+// const q=p.__?__( n=> Math.pow(n, 3))
+
+
+// // Q4: How will you make the url dynamic using the food variable?
+
+// const url = `https://www.themealdb.com/api/json/v1/1/search.php?s={food}` 
+
+// // Q5: How to set image src dynamically? 
+
+
+// const path = "https://www.images.com/cat.jpg"; 
+
+// const div= document.createElement('div');
+// div.innerHTML = `<img src=${path} alt = "">`;
